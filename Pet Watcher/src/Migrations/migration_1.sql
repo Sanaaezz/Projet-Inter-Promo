91c0f39d58bd9f082e8fd461866bb7a0dde6491e
Laurent <laurent.roches.simplon@gmail.com>
2024-08-20 10:38:26 +0200

    passage anglais

--- Pet Watcher/src/Migrations/migration_1.sql
+++ Pet Watcher/src/Migrations/migration_1.sql
@@ -4,73 +4,70 @@
 
 
 #------------------------------------------------------------
-# Table: Animaux
+# Table: pet
 #------------------------------------------------------------
 
-CREATE TABLE Animaux(
-        id_animaux Int  Auto_increment  NOT NULL ,
-        type       Varchar (255) NOT NULL ,
-        max        Int NOT NULL
-	,CONSTRAINT Animaux_PK PRIMARY KEY (id_animaux)
+CREATE TABLE pet(
+        id_pet          Int  Auto_increment  NOT NULL ,
+        type            Varchar (255) NOT NULL ,
+        max             Int NOT NULL
+	,CONSTRAINT pet_PK PRIMARY KEY (id_pet)
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: Role
+# Table: role
 #------------------------------------------------------------
 
-CREATE TABLE Role(
-        id_role Int  Auto_increment  NOT NULL ,
-        type    Varchar (255) NOT NULL
-	,CONSTRAINT Role_PK PRIMARY KEY (id_role)
+CREATE TABLE role(
+        id_role         Int  Auto_increment  NOT NULL ,
+        name            Varchar (255) NOT NULL
+	,CONSTRAINT role_PK PRIMARY KEY (id_role)
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: User
+# Table: user
 #------------------------------------------------------------
 
-CREATE TABLE User(
-        id_user  Int  Auto_increment  NOT NULL ,
-        nom      Varchar (255) NOT NULL ,
-        prenom   Varchar (255) NOT NULL ,
-        mail     Varchar (255) NOT NULL ,
-        password Varchar (255) NOT NULL ,
-        id_role  Int NOT NULL
-	,CONSTRAINT User_PK PRIMARY KEY (id_user)
-
-	,CONSTRAINT User_Role_FK FOREIGN KEY (id_role) REFERENCES Role(id_role)
+CREATE TABLE user(
+        id_user         Int  Auto_increment  NOT NULL ,
+        lastname        Varchar (255) NOT NULL ,
+        fistname        Varchar (255) NOT NULL ,
+        mail            Varchar (255) NOT NULL ,
+        password        Varchar (255) NOT NULL ,
+        id_role         Int NOT NULL
+	,CONSTRAINT user_PK PRIMARY KEY (id_user)
+	,CONSTRAINT user_role_FK FOREIGN KEY (id_role) REFERENCES Role(id_role)
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: Resa
+# Table: reservation
 #------------------------------------------------------------
 
-CREATE TABLE Resa(
-        id_resa     Int  Auto_increment  NOT NULL ,
-        dtm_debut   Datetime NOT NULL ,
-        dtm_fin     Datetime NOT NULL ,
-        commentaire Longtext NOT NULL ,
-        id_user     Int NOT NULL
-	,CONSTRAINT Resa_PK PRIMARY KEY (id_resa)
-
-	,CONSTRAINT Resa_User_FK FOREIGN KEY (id_user) REFERENCES User(id_user)
+CREATE TABLE reservation(
+        id_reservation          Int  Auto_increment  NOT NULL ,
+        dtm_start               Datetime NOT NULL ,
+        dtm_end                 Datetime NOT NULL ,
+        comment                 Longtext NOT NULL ,
+        id_user                 Int NOT NULL
+	,CONSTRAINT reservation_PK PRIMARY KEY (id_reservation)
+	,CONSTRAINT reservation_user_FK FOREIGN KEY (id_user) REFERENCES User(id_user)
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: Resa_details
+# Table: reservation_detail
 #------------------------------------------------------------
 
-CREATE TABLE Resa_details(
-        id_resa_details Int  Auto_increment  NOT NULL ,
-        quantite        Int NOT NULL ,
-        id_resa         Int NOT NULL ,
-        id_animaux      Int NOT NULL
-	,CONSTRAINT Resa_details_PK PRIMARY KEY (id_resa_details)
-
-	,CONSTRAINT Resa_details_Resa_FK FOREIGN KEY (id_resa) REFERENCES Resa(id_resa)
-	,CONSTRAINT Resa_details_Animaux0_FK FOREIGN KEY (id_animaux) REFERENCES Animaux(id_animaux)
+CREATE TABLE reservation_detail(
+        id_reservation_detail   Int  Auto_increment  NOT NULL ,
+        quantity                Int NOT NULL ,
+        id_reservation          Int NOT NULL ,
+        id_pet                  Int NOT NULL
+	,CONSTRAINT reservation_detail_PK PRIMARY KEY (id_reservation_detail)
+	,CONSTRAINT reservation_detail_reservation_FK FOREIGN KEY (id_reservation) REFERENCES reservation(id_reservation)
+	,CONSTRAINT reservation_detail_pet0_FK FOREIGN KEY (id_pet) REFERENCES pet(id_pet)
 )ENGINE=InnoDB;
 
