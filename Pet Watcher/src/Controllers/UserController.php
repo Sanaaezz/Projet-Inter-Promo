4da49e8b858e950d18e4844bc9ee4c257a13e811
Sanaa <sanaa.ezzouine.simplon@gmail.com>
2024-08-20 13:36:35 +0200

    20 aout

--- Pet Watcher/src/Controllers/UserController.php
+++ Pet Watcher/src/Controllers/UserController.php
@@ -1,6 +1,9 @@
 <?php
 namespace src\Controllers;
 
+use src\Models\Database;
+use src\Models\User;
+use src\Repositories\UserRepository;
 use src\Services\Reponse;
 
 class UserControllers{
@@ -13,11 +16,11 @@ class UserControllers{
     $user = (json_decode($data, true));
     if (!empty($user)) {
       $obj = new User($user);
-      $mail = $obj->getEmailUser();
+      $mail = $obj->getMail();
       if (isset($mail) && !empty($mail)) {
         $mail = htmlspecialchars($mail);
       }
-      $password = $obj->getPasswordUser();
+      $password = $obj->getPassword();
       if (isset($password) && !empty($password)) {
         $password = hash("whirlpool", $password);
       }
@@ -25,7 +28,7 @@ class UserControllers{
       $UserRepository = new UserRepository($DbConnexion);
       if ($UserRepository->login($mail, $password)) {
         $_SESSION["connected"] = TRUE;
-        $_SESSION["user"] = serialize($UserRepository->getThisUserByMail($mail));
+        $_SESSION["user"] = serialize($UserRepository->getThisUserById($mail));
         echo HOME_URL . 'dashboard';
         die();
       } else {
@@ -43,20 +46,16 @@ class UserControllers{
     if (!empty($userTableau)) {
       $userObjet = new User($userTableau);
 
-      $lastName = $userObjet->getLastNameUser();
-      $firstName = $userObjet->getFirstNameUser();
-      $address = $userObjet->getAddressUser();
-      $email = $userObjet->getEmailUser();
-      $password = $userObjet->getPasswordUser();
+      $lastName = $userObjet->getLastname();
+      $firstName = $userObjet->getFirstname();
+      $email = $userObjet->getMail();
+      $password = $userObjet->getPassword();
 
       if (isset($lastName) && !empty($lastName)) {
         $lastName = htmlspecialchars($lastName);
       }
       if (isset($firstName) && !empty($firstName)) {
         $firstName = htmlspecialchars($firstName);
-      }
-      if (isset($address) && !empty($address)) {
-        $address = htmlspecialchars($address);
       }
       if (isset($email) && !empty($email)) {
         $email = htmlspecialchars($email);
@@ -68,7 +67,7 @@ class UserControllers{
       $dbConnexion = new Database();
       $UserRepository = new UserRepository($dbConnexion);
 
-      if ($UserRepository->getThisUserByMail($email)) {
+      if ($UserRepository->getThisUserById($email)) {
         header('location: ' . HOME_URL . '?erreur=alreadyUsed');
         die();
       } else {
